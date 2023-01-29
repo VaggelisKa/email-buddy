4c1cdf529c1122dc5d22361a2047b53918a7f2e8
VaggelisKa <e.karavasileiadis@gmail.com>
2023-01-29 20:16:23 +0100

    Initial commit v2

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,10 +1,3 @@
-/**
- * This is the client-side entrypoint for your tRPC API.
- * It's used to create the `api` object which contains the Next.js App-wrapper
- * as well as your typesafe react-query hooks.
- *
- * We also create a few inference helpers for input and output types
- */
 import { httpBatchLink, loggerLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
@@ -18,16 +11,9 @@ const getBaseUrl = () => {
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
-/**
- * A set of typesafe react-query hooks for your tRPC API
- */
 export const api = createTRPCNext<AppRouter>({
   config() {
     return {
-      /**
-       * Transformer used for data de-serialization from the server
-       * @see https://trpc.io/docs/data-transformers
-       **/
       transformer: superjson,
 
       /**
@@ -53,13 +39,5 @@ export const api = createTRPCNext<AppRouter>({
   ssr: false,
 });
 
-/**
- * Inference helper for inputs
- * @example type HelloInput = RouterInputs['example']['hello']
- **/
 export type RouterInputs = inferRouterInputs<AppRouter>;
-/**
- * Inference helper for outputs
- * @example type HelloOutput = RouterOutputs['example']['hello']
- **/
 export type RouterOutputs = inferRouterOutputs<AppRouter>;
