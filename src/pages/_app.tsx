649527fbf28304e2a89c47455f7d09f32a53b35a
VaggelisKa <e.karavasileiadis@gmail.com>
2023-02-04 17:55:05 +0100

    Setting active account upon successful login callback

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,26 @@
 import { type AppType } from "next/app";
 import { api, msalConfig } from "@/utils";
 import { MsalProvider } from "@azure/msal-react";
-import { PublicClientApplication } from "@azure/msal-browser";
+import { EventType, PublicClientApplication } from "@azure/msal-browser";
 import "../styles/globals.css";
 
 const msalInstance = new PublicClientApplication(msalConfig);
+
+const userAccounts = msalInstance.getAllAccounts();
+if (userAccounts?.length > 0) {
+  msalInstance.setActiveAccount(userAccounts[0] ?? null);
+}
+
+msalInstance.addEventCallback((event) => {
+  if (
+    event.eventType === EventType.LOGIN_SUCCESS &&
+    event.payload &&
+    "account" in event.payload &&
+    event.payload.account
+  ) {
+    msalInstance.setActiveAccount(event.payload.account);
+  }
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
