7c6d7149e65e7409bc60214001f9ff438400a0a5
VaggelisKa <e.karavasileiadis@gmail.com>
2023-02-25 20:19:19 +0100

    Fix redirect URI for production

--- src/utils/microsoft-auth.ts
+++ src/utils/microsoft-auth.ts
@@ -4,7 +4,11 @@ import type { Configuration } from "@azure/msal-browser";
 export const msalConfig: Configuration = {
   auth: {
     clientId: "faa244bb-9342-43c3-806b-60b3cac23791",
-    redirectUri: "/emails",
+    redirectUri: `${
+      process.env.NODE_ENV === "production"
+        ? "https://email-buddy.vercel.app"
+        : "http://localhost:3000"
+    }/emails`,
     postLogoutRedirectUri: "/login",
   },
   cache: {
