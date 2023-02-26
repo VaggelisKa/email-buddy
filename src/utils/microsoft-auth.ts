64782b858aed239de36fecda213a4b8501ccf3ae
VaggelisKa <e.karavasileiadis@gmail.com>
2023-02-26 12:25:00 +0100

    Use relative redirect URI

--- src/utils/microsoft-auth.ts
+++ src/utils/microsoft-auth.ts
@@ -4,11 +4,7 @@ import type { Configuration } from "@azure/msal-browser";
 export const msalConfig: Configuration = {
   auth: {
     clientId: "faa244bb-9342-43c3-806b-60b3cac23791",
-    redirectUri: `${
-      process.env.NODE_ENV === "production"
-        ? "https://email-buddy.vercel.app"
-        : "http://localhost:3000"
-    }/emails`,
+    redirectUri: `/emails`,
     postLogoutRedirectUri: "/login",
   },
   cache: {
