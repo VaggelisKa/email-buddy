8c28fe70e6564bd349977c0deac458b87f9c6aef
VaggelisKa <e.karavasileiadis@gmail.com>
2023-02-26 10:52:58 +0100

    Add footer links

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,6 +4,9 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import useFetchUserProfilePhoto from "src/hooks/useFetchUserProfilePhoto";
 import * as UserPlaceholder from "../../public/user-profile-placeholder.svg";
+import { BsTwitter } from "react-icons/bs";
+import { AiFillGithub } from "react-icons/ai";
+import Link from "next/link";
 
 const Header: React.FC = () => {
   const [username, setUsername] = useState("");
@@ -82,19 +85,14 @@ const Footer: React.FC = () => {
           Created by Vaggelis ©{new Date().getFullYear()} - Powered by ChatGPT
         </p>
       </div>
-      {/* <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-        <a>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            className="fill-current"
-          >
-            <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-          </svg>
-        </a>
-      </div> */}
+      <div className="grid-flow-col gap-6 md:place-self-center md:justify-self-end">
+        <Link href="https://twitter.com/vaggelis_kar" target="_blank">
+          <BsTwitter className="h-8 w-8 hover:text-[#1d9bf0] hover:opacity-40" />
+        </Link>
+        <Link href="https://github.com/VaggelisKa/email-buddy" target="_blank">
+          <AiFillGithub className="h-8 w-8 hover:opacity-40" />
+        </Link>
+      </div>
     </footer>
   );
 };
